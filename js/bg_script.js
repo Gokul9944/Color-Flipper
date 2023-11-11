b1513da00c3fa31d962078cf9959ebd7a2492f92
GOKUL M <142975462+Gokul9944@users.noreply.github.com>
2023-11-11 11:05:23 +0530

    Add files via upload
    JS FILE

---
+++ js/bg_script.js
@@ -0,0 +1,31 @@
+const color=document.getElementById("colorx");
+const  wrap=document.getElementById("wrap");
+const btn1=document.getElementById("btn");
+let btn2=document.getElementById("btn2");
+let input=document.getElementById("inpout");
+//hex-color
+const  hex_color=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+
+btn1.addEventListener('click',colorg)
+function colorg()
+{
+    let g='#'
+    for(let i=0;i<6;i++)
+    {
+        g=g+randomColor()
+    }
+    wrap.style.backgroundColor=g
+    color.innerHTML=g
+}
+
+ function colorxx()  
+{
+    
+}
+
+function randomColor()
+{
+   let goku=Math.floor(Math.random()*16)
+   return hex_color[goku]
+}
+
